915996427c674ed7dc29fcdcd594b012368ab6c7
matthewchavis8 <matthewchavis8@gmail.com>
2025-09-19 23:26:53 -0500

    Unit Test  - Testing print() and println() with just regular strings  - Testing print and println with integers  - Note: Test for printing multiple arguments is failing  - Will write some more later

--- test/print.cpp
+++ test/print.cpp
@@ -1,9 +1,71 @@
+#include "gtest/gtest.h"
 #include <gtest/gtest.h>
+#include <string_view>
+#include "../Toolbox/print.hpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloWorld, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+using namespace testing;
+
+TEST(ToolBoxPrint, PrintString) {
+  
+  internal::CaptureStdout();
+
+  print("Hello World!");
+
+  std::string_view output { internal::GetCapturedStdout() };
+  std::string_view expected { "Hello World!" };
+
+  EXPECT_EQ(output, expected);
 }
+
+TEST(ToolBoxPrint, PrintStringNewline) {
+  
+  internal::CaptureStdout();
+
+  println("Hello World!");
+
+  std::string_view output { internal::GetCapturedStdout() };
+  std::string_view expected { "Hello World!\n" };
+
+  EXPECT_EQ(output, expected);
+}
+
+TEST(ToolBoxPrint, PrintInteger) {
+  
+  internal::CaptureStdout();
+
+  println("The number is: {}", 5);
+
+  std::string_view output { internal::GetCapturedStdout() };
+  std::string_view expected { "The number is: 5\n" };
+
+  EXPECT_EQ(output, expected);
+}
+
+TEST(ToolBoxPrint, PrintIntegerNewline) {
+  
+  internal::CaptureStdout();
+
+  print("The number is: {}", 5);
+
+  std::string_view output { internal::GetCapturedStdout() };
+  std::string_view expected { "The number is: 5" };
+
+  EXPECT_EQ(output, expected);
+}
+
+// NOTE: this one fails here it is not printing each sequentially
+// TEST(ToolBoxPrint, PrintMultipleInputs) {
+//
+//   internal::CaptureStdout();
+//
+//   print("The number is: {}", 5, 6, 7);
+//
+//   std::string_view output { internal::GetCapturedStdout() };
+//   std::string_view expected { "The number is: 5 6 7" };
+//
+//   SCOPED_TRACE(::testing::Message()
+//     << "Output: " << output << '\n'
+//     << "Expected: " << expected << '\n');
+//
+//   EXPECT_EQ(output, expected);
+// }
